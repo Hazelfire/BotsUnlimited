8145fb4aa8f307fdac8bb6dc998d2c35165d1810
Sam Nolan <samnolan555@gmail.com>
2019-01-19 21:35:50 +1100

    Add Stacy to give details about missing information

--- Stacy/index.py
+++ Stacy/index.py
@@ -36,9 +36,27 @@ async def on_message(message):
                 await client.send_typing(message.channel)
                 events = get_upcoming('rmit.edu.au_ct5j003ncdbmu1o2ga2gasu86k@group.calendar.google.com');
                 if len(events) > 0:
-                    upcoming = "\n".join([event_to_string(event) for event in events])
+                    upcoming = "\n".join([event_to_string(event) for event in events if 'dateTime' in event['start'] and 'description' in event and 'location' in event])
+                    messages = []
                     counter = 0
-                    await client.send_message(message.channel, content='Here are the events we have planned: \n {}'.format(upcoming))
+                    if len(upcoming) > 0:
+                        messages.append('Here are the events we have planned: \n {}'.format(upcoming))
+                    else: 
+                        messages.append('We don\'t actually have any events completely ready to publish yet')
+
+                    no_time = "\n".join([event_to_string(event) for event in events if 'dateTime' not in event['start']])
+                    
+                    if len(no_time) > 0:
+                        messages.append('These ones we haven\'t got a specific time for: \n {}'.format(no_time))
+
+                    no_description = "\n".join([event_to_string(event) for event in events if 'description' not in event])
+                    if len(no_description) > 0:
+                        messages.append('These ones don\'t have a description: \n {}'.format(no_description))
+
+                    no_location = "\n".join([event_to_string(event) for event in events if 'location' not in event])
+                    if len(no_description) > 0:
+                        messages.append('These ones don\'t have a location: \n {}'.format(no_location))
+                    await client.send_message(message.channel, content="\n\n".join(messages))
                 else:
                     await client.send_message(message.channel, "No events planned")
             elif words_in(['meeting'], message.content):
