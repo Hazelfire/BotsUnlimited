3df376c62c743995ee382c00eb78ad1c63e16e94
Sam Nolan <samnolan555@gmail.com>
2019-07-09 11:19:38 +1000

    Change stuff around

--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(
-    name="BotsUnlimited",
-    version="0.1",
-    packages=find_packages(),
-    setup_requires=["pytest-runner"],
-    tests_require=["pytest"],
-)
